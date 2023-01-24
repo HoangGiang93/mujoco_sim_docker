4befeb7c0a28a3c04fc018fcabb0ae7c64675590
Giang Nguyen <hoanggia@uni-bremen.de>
2023-01-24 16:27:24 +0100

    Add giskard and change service name

---
+++ images/giskard-ws-image/entrypoint.bash
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /home/giskard_ws/devel/setup.bash
+exec "$@"
--- setup_ros_packages.bash
+++ setup_ros_packages.bash
@@ -3,3 +3,8 @@ cd images/mujoco-ws-image/src
 wstool update
 cd mujoco_sim
 make
+
+cd images/giskard-ws-image/src
+wstool update
+cd ../
+make
