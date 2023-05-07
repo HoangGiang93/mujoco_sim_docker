361ef226337294de08514ea881077e76dfcb7fc8
Giang Nguyen <hoanggia@uni-bremen.de>
2023-05-07 04:42:07 +0200

    Update

--- setup_ros_packages.bash
+++ setup_ros_packages.bash
@@ -1,7 +1,5 @@
 #!/bin/bash
 CURRENT_DIR=${PWD}
-cd ${CURRENT_DIR}/images/mujoco-ws-image/usd
-git clone git@github.com:HoangGiang93/USD.git --branch UsdOntology
 cd ${CURRENT_DIR}/images/mujoco-ws-image/src
 wstool update 
 cd ${CURRENT_DIR}/images/mujoco-ws-image/src/mujoco_sim
